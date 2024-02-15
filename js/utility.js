8b04ae10f6a07374ac5895ddaa1f4a3b68f803c4
anissarkeraraf <anissarkeraraf@gmail.com>
2024-02-15 14:54:38 +0600

    added tha js

---
+++ js/utility.js
@@ -0,0 +1,33 @@
+function hiddenElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+// function getARandomAlphabeta(){
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+
+
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+//     console.log(index);
+// }
+function getARandomAlphabeta(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-orange-400");
+}
